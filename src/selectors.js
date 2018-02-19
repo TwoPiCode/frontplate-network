de230e17094d2abb720ae25487110d6ea5f17b8f
Peter Boyer <petertboyer@gmail.com>
2018-02-19 14:17:39 +1100

    Clean up http status color selector

--- src/selectors.js
+++ src/selectors.js
@@ -19,18 +19,18 @@ export const selectBodyJSON = (body: Object|string): string => {
 */
 
 export const selectStatusColor = (status: number) => {
-  if (status >= 200 && status < 300) {
+  if (status >= 100 && status < 200) {
+    // 100s - informational
+    return '#afafaf'
+  } else if (status >= 200 && status < 300) {
     // 200s - ok
     return '#00ae25'
+  } else if (status >= 300 && status < 400) {
+    // 300s - redirection
+    return '#ab00ff'
   } else if (status >= 400 && status < 500) {
     // 400s - client error
     return '#ff8200'
-  } else if (status >= 100 && status < 200) {
-    // 300s - redirection
-    return '#ab00ff'
-  } else if (status >= 100 && status < 200) {
-    // 100s - informational
-    return '#f100ff'
   } else {
     // 500s - server error
     return '#f00000'
